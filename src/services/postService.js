0366511bbed62991dd7da3c535faa1ade5ad3134
Soni-WebDev <soni.cloudhub@gmail.com>
2025-03-12 12:17:58 +0530

    add aws config

---
+++ src/services/postService.js
@@ -0,0 +1,7 @@
+export const createPost = async (createPostObj)=>{
+    // 1. take the img of the post and upload on aws
+    // 2. get the urlof the img from the aws response
+    // 3. create a post with the caption and the img url in the db using repositiry
+    // 4. return the post obj
+    
+}
